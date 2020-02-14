2c2bb0f61466629dbfa720ed852c2737937dad5d
Emilio Feliciano <FelicianoEJ@users.noreply.github.com>
2020-02-14 17:52:53 -0600

    Dania ask

--- cmd/horoscoped-web/horoscoped-web.go
+++ cmd/horoscoped-web/horoscoped-web.go
@@ -1,22 +1,33 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"html/template"
 	"net/http"
+
+	"github.com/200106-uta-go/goloardo-project-2/pkg/gethoroscope"
 )
 
+// IndexContent ...
+type IndexContent struct {
+	Horoscopes []gethoroscope.DailyHoroscope
+}
+
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("web")))
 
-	http.HandleFunc("/getdaily", func(w http.ResponseWriter, r *http.Request) {
+	itmpl := template.Must(template.ParseFiles("web/index.html"))
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		data := IndexContent{
+			Horoscopes: gethoroscope.GetAllDailyHoroscope(),
+		}
+		itmpl.Execute(w, data)
 	})
 
 	println("Server is running on port 80")
 	err := http.ListenAndServe(":80", nil)
-
 	if err != nil {
-		log.Fatal(err)
+		fmt.Println("Server crash:", err)
 	}
-
 }
