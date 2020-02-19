fe92dccacdebd39f4b41e6da67bbefc62052311a
CodeZipline <jnguyen5039@gmail.com>
2020-02-18 21:44:19 -0600

    Created a shell script to develope manager node for ec2 instances.

---
+++ managerinit.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#become root user
+sudo su
+#upgrade basic packages for a new start of a instance
+apt update
+apt upgrade
+#download java development kit
+apt install openjdk-8-jdk
+#download jenkins with a key
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+#append this to sources list
+echo -e "deb https://pkg.jenkins.io/debian binary/\n" >> /etc/apt/sources.list
+#UPDATE again due to newly downloaded jenkins
+apt update
+#install jenkins
+apt install jenkins
+#install go and docker
+apt install golang
+apt install docker.io
+
+
+export MYIP=`curl ifconfig.me`
+#init a docker swarm 
+docker swarm init --advertise-addr $MYIP
+#docker swarm join-token worker
+
+docker pull felicianoej/arkcontroller
+docker pull codezipline/dbserver
+docker pull danish287/horsocoped
+docker network create -d overlay --attachable onet
+
+#run arkcontrol
+docker service create --name arkcontroller --publish published=7777,target=7777 --mode replicated --replicas=1 --network onet felicianoej/arkcontroller
+
+
+
+
+
+
