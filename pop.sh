ef93b07e4e67addeb9b37b969b4eacdeb08ddca0
Joshua Nguyen <jnguyen5039@gmail.com>
2020-02-20 13:53:46 -0700

    extract db ip for populate.sh

--- pop.sh
+++ pop.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
 
 # Needs ip for database(private)
-docker service create --name pop --network onet --mode replicated --replicas=1 --restart-condition none felicianoej/p2dbpop -ip=
+export DBIP=`sudo docker service inspect dbserver | grep Addr | tail -1 | sed 's/"Addr": "//' | sed 's/"//' | cut -f1 -d"/" | sed -e 's/^[ \t]*//'`
+docker service create --name pop --network onet --mode replicated --replicas=1 --restart-condition none felicianoej/p2dbpop -ip=$DBIP
