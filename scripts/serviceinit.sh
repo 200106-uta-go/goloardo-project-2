6f1f4b2aad5c82365d5432cee653efe7f3266e81
Joshua Nguyen <jnguyen5039@gmail.com>
2020-02-20 15:17:24 -0700

    clean up servicesinit.sh

--- scripts/serviceinit.sh
+++ scripts/serviceinit.sh
@@ -3,4 +3,3 @@
 # Must replace the ip with private ip of the manager 
 docker service create --name dbserver --publish published=8081,target=8081 --mode replicated --replicas=1 --network onet codezipline/dbserver -ark=10.0.1.2
 docker service create --name horoscoped --publish published=80,target=8080 --mode replicated --replicas=5 --network onet danish287/horsocoped -ark=10.0.1.2
-docker service create --name pop --network onet --mode replicated --replicas=1 --restart-condition none felicianoej/p2dbpop -ip=10.0.0.2
